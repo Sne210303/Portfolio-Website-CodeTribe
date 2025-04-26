0624821728475122117b232131a9710faa6e7efd
Snenhlanhla <snenhlanhlamkhwanazi24@gmail.com>
2025-04-26 20:25:42 +0200

    Made the pop up message even more proffessional looking

--- script.js
+++ script.js
@@ -5,25 +5,37 @@ menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
 
-
 const contactForm = document.getElementById('contact-form');
 
 if (contactForm) {
     contactForm.addEventListener('submit', function(event) {
         event.preventDefault(); 
         
+        
         Swal.fire({
-            title: 'Thank You!',
-            text: "Your message has been received. We'll be in touch soon!",
-            icon: 'success',
-            showConfirmButton: false, 
-            timer: 3000, 
-            timerProgressBar: true, 
+            title: 'Sending your message...',
+            html: 'Please wait a moment.',
+            allowOutsideClick: false,
+            didOpen: () => {
+                Swal.showLoading();
+            }
         });
 
-        contactForm.reset();
+        
+        setTimeout(() => {
+            Swal.fire({
+                title: 'Thank You!',
+                text: "Your message has been received. We'll be in touch soon!",
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+            });
+            contactForm.reset(); 
+        }, 2000); 
     });
 }
+
 
 
 
